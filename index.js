224ab604a9bb749775228fbb6ba0e21080e2a136
Andrii Chychkan <mywork1728@gmail.com>
2024-08-21 17:24:18 +0300

    added get tours api

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world');
-});
 
 const port = 8000;
 
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
