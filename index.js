5ad0eaecaede85d2cb743f90a6bd6fbbd9533d68
Andrii Chychkan <mywork1728@gmail.com>
2024-08-22 11:53:01 +0300

    added tour by id

--- index.js
+++ index.js
@@ -44,3 +44,21 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
