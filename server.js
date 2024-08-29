ca43086cc0c33d7df362cda55c1534c74835cb6d
Andrii Chychkan <mywork1728@gmail.com>
2024-08-29 17:20:15 +0300

    Develop (#11)

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successfull');
+});
 
 const app = require('./app');
 
