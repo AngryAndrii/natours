b54c0c6e2708b1a9a6b65dd478540eea9449f5a3
Andrii Chychkan <mywork1728@gmail>
2024-08-30 00:24:04 +0300

    added tour schema

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection successfull');
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const app = require('./app');
 
 const port = process.env.PORT;
